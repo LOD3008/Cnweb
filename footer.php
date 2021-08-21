7989b35d045c9c3b29afa93b9567c84d58b5f02a
LOD3008 <89307456+LOD3008@users.noreply.github.com>
2021-08-21 21:07:32 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,118 @@
+<div class="main-8">
+      <h3 class="text-center">ĐỐI TÁC CỦA HANOI APTECH </h3>
+      <div id="carouselExampleSlidesOnly" class="carousel slide my-5 mx-4" data-bs-ride="carousel">
+        <div class="carousel-inner container">
+          <div class="carousel-item active">
+          <!--Grid row--> 
+            <div class="row">
+              <div class= "col-md-3 ">
+                <img src="images/ncc.png" class="w-50" alt="...">
+              </div>
+              <div class="col-md-3">
+                <img src="images/logo-misa.svg" class="w-50 pt-3" alt="...">
+              </div>
+              <div class="col-md-3">
+                <img src="images/Middlesex-University-Logo_s-1-1-600x400-1.jpg" class="w-50 " alt="...">
+              </div>
+              <div class="col-md-3">
+                <img src="images/unnamed.png" class="w-100 h-30 pt-4" alt="...">
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item h-100">
+            <div class="row">
+              <div class="col-md-3">
+                <img src="images/logo.png" class="w-50" alt="...">
+              </div>
+              <div class="col-md-3">
+                <img src="images/Samsung_Logo.jpg" class="w-50 pt-3" alt="...">
+              </div>
+              <div class="col-md-3">
+                <img src="images/logo-vt.png" class="w-30 pt-4" alt="...">
+              </div>
+              <div class="col-md-3">
+                <img src="images/logo-1.png" class="w-45 pt-3" alt="...">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+
+    <footer class="footer1 text-center ">
+      <!-- Grid container -->
+        <div class="container p-4 text-white">
+        <!--Grid row--> 
+          <div class="row">
+          <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mt-5 text-lg-start">
+            <h5 class="text-uppercase display-9 mb-4">Khóa học sắp khai giảng</h5>
+              <ul class="list-unstyled">
+                <li class="mb-3">
+                  <a href="#" class="text-white text-decoration-none">Lập trình viên Quốc Tế</a>
+                </li>
+                <li class="mb-3">
+                  <a href="#" class="text-white text-decoration-none">Lập trình Web với PHP</a>
+                </li>
+                <li>
+                  <a href="#" class="text-white text-decoration-none">Khóa học Lập trình Java</a>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+        
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 my-5 mb-md-0 text-lg-start">
+              <h5 class="text-uppercase mb-4">Liên hệ</h5>
+        
+              <ul class="list-unstyled">
+                <li>
+                  <p class="text-white">Tầng 1, Tòa nhà C5, Đại học Thủy Lợi, 175 Tây Sơn, Trung Liệt, Đống Đa, Hà Nội</p>
+                </li>
+                <li>
+                  <p class="text-white">Tel: (024) 3563 7511</p>
+                </li>
+                <li>
+                  <p class="text-white">Hotline: 038 2020 858</p>
+                </li>
+                <li>
+                  <p class="text-white">Mail: info@aptech.vn</p>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+        
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 my-5 mb-md-0 ">
+              <h5 class="text-uppercase text-start">Đăng kí tư vấn</h5>
+              <ul class="list-unstyled">
+                <li>
+                  <input type="text" class="form-control mt-4" id="exampleFormControlInput1" placeholder="Họ và tên *">
+                </li>
+                <li>
+                  <input type="text" class="form-control mt-4" id="exampleFormControlInput1" placeholder="Số điện thoại *">
+                </li>
+                <li>
+                  <input type="text" class="form-control mt-4" id="exampleFormControlInput1" placeholder="Email *">
+                </li>
+                <li>
+                  <input type="text" class="form-control mt-4" id="exampleFormControlInput1" placeholder="Địa chỉ *">
+                </li>
+                <li>
+                  <button type="submit" class="btn btn-success mt-4">Đăng kí</button>
+                </li>
+              </ul>
+            </div>
+            <!--Grid column-->
+          </div>
+          <!--Grid row-->
+        </div>
+        <!-- Grid container -->
+    </footer> 
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hanoi Aptech- Trường Đào Tạo Lập Trình Viên Quốc Tế</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  </head>
+  <body>
+  <header class="">
+      <nav class="navbar fixed-top navbar-expand-md navbar-light bg-white">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="images/Aptech Logo.png" alt="Aptech Logo" class="img-responsive">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-6 ">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">TRANG CHỦ</a>
+              </li>
+              
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  APTECH-HANOI
+                </a>
+                <ul class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item " href="#">GIỚI THIỆU</a></li>
+                  <li><a class="dropdown-item " href="#">CƠ SỞ VẬT CHẤT</a></li>
+                  <li><a class="dropdown-item " href="#">GIẢNG VIÊN</a></li>
+                  <li><a class="dropdown-item " href="#">HỎI ĐÁP</a></li>
+                  <li><a class="dropdown-item " href="#">LIÊN HỆ</a></li>
+                </ul>
+              </li>
+              
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  CHƯƠNG TRÌNH ĐÀO TẠO
+                </a>
+                <ul class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item " href="#">LẬP TRÌNH VIÊN QUỐC TẾ</a></li>
+                  <li><a class="dropdown-item " href="#">LẬP TRÌNH WEB VỚI PHP</a></li>
+    
+                </ul>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">LIÊN THÔNG</a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  TIN TỨC
+                </a>
+                <ul class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item " href="#">TIN TỨC VỀ CNTT</a></li>
+                  <li><a class="dropdown-item " href="#">LỊCH THI</a></li>
+                  <li><a class="dropdown-item " href="#">LỊCH TUYỂN SINH</a></li>
+                  <li><a class="dropdown-item " href="#">THÔNG TIN HỌC BỔNG</a></li>
+                  <li><a class="dropdown-item " href="#">GƯƠNG MẶT TIÊU BIỂU</a></li>
+                </ul>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  TUYỂN DỤNG
+                </a>
+                <ul class="dropdown-menu bg-light" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item " href="#">TIN TUYỂN DỤNG</a></li>
+                  <li><a class="dropdown-item " href="#">GÓC NGHỀ NGHIỆP</a></li>
+                  <li><a class="dropdown-item " href="#">GIẢI TRÍ</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
